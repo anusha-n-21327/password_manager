43b307c63f943b3d8f252480fb6bfe467a4b4459
[dyad] <itzanu.cyber@gmail.com>
2025-09-01 19:28:50 +0530

    [dyad] Added background particle animation - wrote 5 file(s), added react-tsparticles, tsparticles-slim package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { VaultProvider, useVault } from './context/VaultContext';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import { ParticlesBackground } from './components/ParticlesBackground';
 
 const AppContent = () => {
   const { isUnlocked } = useVault();
@@ -11,13 +12,16 @@ const AppContent = () => {
 };
 
 const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <VaultProvider>
-      <AppContent />
-    </VaultProvider>
-  </TooltipProvider>
+  <>
+    <ParticlesBackground />
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <VaultProvider>
+        <AppContent />
+      </VaultProvider>
+    </TooltipProvider>
+  </>
 );
 
 export default App;
